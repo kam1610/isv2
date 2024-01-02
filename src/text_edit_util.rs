f27727a173d75634eb116d878a91e5b0b5bb43e6
kam1610 <936120+kam1610@users.noreply.github.com>
2024-01-02 20:15:36 +0900

    workaround for kill line

--- src/text_edit_util.rs
+++ src/text_edit_util.rs
@@ -31,6 +31,8 @@ pub mod text_edit{
     pub fn act_cursor_move(win: ApplicationWindow) -> SimpleAction{
         let act = SimpleAction::new(ACT_CURSOR_MOVE, Some(&VariantTy::INT32));
         act.connect_activate(move|_act, val|{
+            let current_focus =
+                if let Some(f) = GtkWindowExt::focus(&win) {f} else {return;};
             let val = val.expect("expect val").get::<i32>().expect("couldn't get i32 val");
             let val = match val {
                 x if x == ActCursorCmd::FwdChar   as i32 => {(MovementStep::VisualPositions,  1)},
@@ -46,8 +48,6 @@ pub mod text_edit{
                 _ => { println!("(act_cursor_move) unexpected val: {}", val); return; }
             };
 
-            let current_focus =
-                if let Some(f) = GtkWindowExt::focus(&win) {f} else {return;};
             if let Some(view) = current_focus.downcast_ref::<TextView>(){
                 view.emit_move_cursor(val.0, val.1, false);
             } else if let Some(text) = current_focus.downcast_ref::<gtk::Text>(){
@@ -60,18 +60,25 @@ pub mod text_edit{
     pub fn act_delete_text(win: ApplicationWindow) -> SimpleAction{
         let act = SimpleAction::new(ACT_DEL_TEXT, Some(&VariantTy::INT32));
         act.connect_activate(move|_act, val|{
+            let current_focus =
+                if let Some(f) = GtkWindowExt::focus(&win) {f} else {return;};
             let val = val.expect("expect val").get::<i32>().expect("couldn't get i32 val");
             let val = match val {
                 x if x == ActDelTextCmd::DelBackChar  as i32 => {(DeleteType::Chars,           -1)},
                 x if x == ActDelTextCmd::DelChar      as i32 => {(DeleteType::Chars,            1)},
-                x if x == ActDelTextCmd::KillLine     as i32 => {(DeleteType::DisplayLineEnds,  1)},
+                x if x == ActDelTextCmd::KillLine     as i32 => {
+                    // workaround
+                    if let Some(view) = current_focus.downcast_ref::<TextView>(){
+                        view.emit_move_cursor(MovementStep::DisplayLineEnds, 1, true);
+                        view.emit_delete_from_cursor(DeleteType::DisplayLineEnds, 1);
+                    }
+                    return;
+                },
                 x if x == ActDelTextCmd::BackKillWord as i32 => {(DeleteType::WordEnds,        -1)},
                 x if x == ActDelTextCmd::KillWord     as i32 => {(DeleteType::WordEnds,         1)},
                 _ => { println!("(act_edit_text) unexpected val: {}", val); return; }
             };
 
-            let current_focus =
-                if let Some(f) = GtkWindowExt::focus(&win) {f} else {return;};
             if let Some(view) = current_focus.downcast_ref::<TextView>(){
                 view.emit_delete_from_cursor(val.0, val.1);
             } else if let Some(text) = current_focus.downcast_ref::<gtk::Text>(){
